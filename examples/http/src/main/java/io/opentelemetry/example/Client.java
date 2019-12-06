2421eb43168b8be0a20361940863c3b0eb18f89c
Giovanni Liva <giovanni.liva@dynatrace.com>
2019-12-06 15:59:19 +0100

    Add HTTP example

---
+++ examples/http/src/main/java/io/opentelemetry/example/Client.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.opentelemetry.example;
+
+import io.opentelemetry.context.propagation.HttpTextFormat;
+import io.opentelemetry.exporters.inmemory.InMemorySpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SpanData;
+import io.opentelemetry.sdk.trace.TracerSdkFactory;
+import io.opentelemetry.sdk.trace.export.SimpleSpansProcessor;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Status;
+import io.opentelemetry.trace.Tracer;
+import io.opentelemetry.trace.propagation.HttpTraceContext;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Client {
+
+    static int port = 8080;
+    // OTel API
+    Tracer OTel;
+    // Export traces in memory
+    InMemorySpanExporter inMemexporter = InMemorySpanExporter.create();
+    // Share context via http headers
+    HttpTextFormat textFormat = new HttpTraceContext();
+    // Inject context into the request
+    HttpTextFormat.Setter<HttpURLConnection> setter = new HttpTextFormat.Setter<HttpURLConnection>() {
+        @Override
+        public void put(HttpURLConnection carrier, String key, String value) {
+            carrier.setRequestProperty(key, value);
+        }
+    };
+
+    private void initTracer(){
+        // Get the tracer
+        TracerSdkFactory tracer = OpenTelemetrySdk.getTracerFactory();
+        // Set to process in memory the spans
+        tracer.addSpanProcessor(
+                SimpleSpansProcessor.newBuilder(inMemexporter).build()
+        );
+        // Give the name to the traces
+        OTel = tracer.get("example/http/client");
+    }
+
+    public Client() throws Exception {
+        initTracer();
+
+        // Connect to the server locally
+        URL url = new URL("http://127.0.0.1:" + port);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        // Start a span
+        Span span = OTel.spanBuilder("Request hello API").startSpan();
+        // Inject the request with the context
+        textFormat.inject(span.getContext(), con, setter);
+
+        // Process the request
+        con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        // Close the Span
+        span.setStatus(Status.OK);
+        span.end();
+
+        // Output the result of the request
+        System.out.println("Response Code: " + status);
+        System.out.println("Response Msg: " + content);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        // Perform request every 5s
+        Thread t = new Thread(() -> {
+            while(true) {
+                try {
+                    Client c = new Client();
+                    Thread.sleep(5000);
+                    for (SpanData spanData : c.inMemexporter.getFinishedSpanItems()) {
+                        System.out.println("  - " + spanData);
+                    }
+                    c.inMemexporter.reset();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace(System.out);
+                }
+            }
+        });
+        t.start();
+    }
+}
---
+++ examples/http/src/main/java/io/opentelemetry/example/Server.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.opentelemetry.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import io.opentelemetry.OpenTelemetry;
+import io.opentelemetry.context.propagation.HttpTextFormat;
+import io.opentelemetry.distributedcontext.DistributedContextManager;
+import io.opentelemetry.exporters.inmemory.InMemorySpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SpanData;
+import io.opentelemetry.sdk.trace.TracerSdkFactory;
+import io.opentelemetry.sdk.trace.export.SimpleSpansProcessor;
+import io.opentelemetry.trace.AttributeValue;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.SpanContext;
+import io.opentelemetry.trace.Tracer;
+import io.opentelemetry.trace.propagation.HttpTraceContext;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Server {
+
+    private class helloHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            try {
+                // Extract the context from the request
+                SpanContext ctx = extractCtx(he);
+
+                // Build a span based on the received context
+                Span span = OTel
+                        .spanBuilder("hello handler")
+                        .setParent(ctx)
+                        .startSpan();
+
+                // Process the request
+                answer(he, span);
+
+                // Close the span
+                span.end();
+            } catch (StringIndexOutOfBoundsException e) {
+                // msg without ctx
+            } catch(Exception e){
+                // catch other errors
+                System.out.println(e.getMessage());
+            }
+        }
+
+        private SpanContext extractCtx(HttpExchange he) throws IOException {
+            SpanContext ctx = (SpanContext) textFormat.extract(he, getter);
+            return ctx;
+        }
+
+        private void answer(HttpExchange he, Span span) throws IOException {
+            String response = "Hello World!";
+            he.sendResponseHeaders(200, response.length());
+            OutputStream os = he.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            System.out.println("Served Client: " + he.getRemoteAddress());
+            Map<String, AttributeValue> event = new HashMap<>();
+            event.put(
+                    "client.info",
+                    AttributeValue.stringAttributeValue(he.getRemoteAddress().getHostString())
+            );
+            // Attach some events to the request
+            span.addEvent("event", event);
+        }
+    }
+
+    HttpServer server;
+    static int port = 8080;
+
+    // OTel API
+    Tracer OTel;
+    // Export traces in memory
+    InMemorySpanExporter inMemexporter = InMemorySpanExporter.create();
+    // Receive context via http headers
+    HttpTextFormat.Getter<HttpExchange> getter = new HttpTextFormat.Getter<HttpExchange>() {
+        @Override
+        public String get(HttpExchange carrier, String key) {
+            if(carrier.getRequestHeaders().containsKey(key)){
+                return carrier.getRequestHeaders().get(key).get(0);
+            }
+            return "";
+        }
+    };
+    // Extract context from the HTTP request
+    HttpTextFormat textFormat = new HttpTraceContext();
+
+    public Server() throws IOException {
+        this(port);
+    }
+
+    public Server(int port) throws IOException {
+        initTracer();
+        server = HttpServer.create(new InetSocketAddress(port), 0);
+        //Test urls
+        server.createContext("/", new helloHandler());
+        server.start();
+        System.out.println("Server ready on http://127.0.0.1:" + port);
+    }
+
+    private void initTracer(){
+        // Get the tracer
+        TracerSdkFactory tracer = OpenTelemetrySdk.getTracerFactory();
+        // Set to process in memory the spans
+        tracer.addSpanProcessor(
+                SimpleSpansProcessor.newBuilder(inMemexporter).build()
+        );
+        // Give the name to the traces
+        OTel = tracer.get("example/http/server");
+    }
+
+    private void stop(){
+        server.stop(0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server s = new Server();
+        // Gracefully close the server
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> s.stop()));
+        // Print new traces every 1s
+        Thread t = new Thread(() -> {
+            while(true) {
+                try {
+                    Thread.sleep(1000);
+                    for (SpanData spanData : s.inMemexporter.getFinishedSpanItems()) {
+                        System.out.println("  - " + spanData);
+                    }
+                    s.inMemexporter.reset();
+                } catch (Exception e) {  }
+            }
+        });
+        t.start();
+    }
+}
